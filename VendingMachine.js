39a39f34f975007f731eb4d36ad68de64304121e
Yuya Harada <tzou11291996@gmail.com>
2020-04-07 21:47:24 +0900

    Our first commit
    Co-authored-by: maegatro <maegatro@users.noreply.github.com>
    Co-authored-by: rikukawano <rikukawano@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,4 +1,79 @@
 // your class here
+module.exports = class VendingMachine {
+  constructor() {
+    this.till = {
+      "10": 0,
+      "50": 0,
+      "100": 0,
+      "500": 0,
+    };
+    this.balance = 0;
+
+    this.data = {
+      row: "",
+      column: "",
+      totalsoins: 0,
+    }; //okkkkkkkkkkk
+
+    const appleJuice = { name: `Apple Juice`, price: 350, count: 5 };
+    const coffee = { name: "Tully's", price: 250, count: 7 };
+    const greenTea = { name: "Green Tea", price: 150, count: 4 };
+    const beer = { name: "Corona", price: 350, count: 7 };
+    const calpis = { name: "Calpis", price: 150, count: 10 };
+    const coke = { name: "Coke", price: 150, count: 10 };
+    const kombucha = { name: "Kombucha", price: 150, count: 10 };
+    const tea = { name: "Jasmine Tea", price: 150, count: 10 };
+    const corona = { name: "Corona", price: 150, count: 10 };
+    const fanta = { name: "Fanta", price: 150, count: 10 };
+    const sapporo = { name: "Sapporo", price: 150, count: 10 };
+    const water = { name: "Water", price: 150, count: 10 };
+
+    this.inventory = {
+      A: [appleJuice, coffee, greenTea, beer],
+      B: [calpis, coke, kombucha, tea],
+      C: ["Corona", "Fanta", "Sapporo", "water"],
+      D: ["Corn Soup", "RedBull", "Lemonade", "Ginger Ale"],
+    };
+  }
+
+  getBalance() {
+    const obj = this.till;
+    let value = 0;
+    for (let key in obj) {
+      value += Number(key) * Number(obj[key]);
+    }
+    return (this.balance = value);
+  }
+
+  // getDrink (this.data){
+  //   return drink;
+  // }
+
+  insertCoin(coin) {
+    switch (coin) {
+      case 10:
+        this.till["10"] += 1;
+        break;
+      case 50:
+        this.till["50"] += 1;
+        break;
+      case 100:
+        this.till["100"] += 1;
+        break;
+      case 500:
+        this.till["500"] += 1;
+        break;
+      default:
+        console.error("Please insert a valid coin.");
+    }
+    this.getBalance();
+  }
+
+  pressButton(row, column) {
+    console.log(row);
+    return this.inventory[row][column];
+  }
+};
 /*
   >>> Don't forget to use module.exports!
   What is that? Well, glad you asked.
--- test/test.js
+++ test/test.js
@@ -1,5 +1,15 @@
 const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
+// DONE => Given that the balance is zero, when a coin is inserted, then the balance should rise and types of coins should be stored
+// Given that no row is selected, when a row is selected the letter should be saved and printed to the console
+// Given that a row is selected, when there is sufficient balance and inventory and a column is selected
+// then the row and column should be logged to the console
+// and a message should be logged stating "Here is your [item name]"
+// and the item inventory should decrease by 1
+// and the correct change should be returned (log type and number of coins to console)
+// Given that a row and column are selected, when there is no inventory at that column, then an error message should be logged.
+// Given that a row and column are selected, when the balance is insufficient to purchase the selected item, then an error message should be printed
+// Given that the program has just started, when the balance is read, then it should read zero
 
 describe("vending machine", () => {
   it("should accept valid coins", () => {
@@ -16,6 +26,22 @@ describe("vending machine", () => {
       100: 0,
       500: 1,
     });
+
     expect(machine.balance).to.equal(500); // Use an ES6 getter
   });
+
+  it("should print letter to console when row is selected", () => {
+    // Setup
+    const machine = new VendingMachine();
+
+    // Exercise
+    const drink = machine.pressButton("A", 2);
+
+    // Assert
+    expect(drink.name).to.equal("Green Tea");
+  });
 });
+
+// Hint: do not try to test what is being logged to console, instead think about how to
+// structure your methods and have them return objects for things such as returned change
+// or dispensed item, this then allows you to test the return value
